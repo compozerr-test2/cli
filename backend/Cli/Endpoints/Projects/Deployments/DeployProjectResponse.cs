4c01a8518050292fa6d2eb68e59dd2d476e4c16a
Milo <tomilobas@gmail.com>
2025-05-28 23:01:42 +0200

    Moved DeployProjectCommand and added handling

--- backend/Cli/Endpoints/Projects/Deployments/DeployProjectResponse.cs
+++
@@ -1,3 +0,0 @@
-namespace Cli.Endpoints.Projects.Deployments;
-
-public sealed record DeployProjectResponse(string StatusUrl);
--- backend/Cli/Endpoints/Projects/Deployments/DeploymentQueuedEvent.cs
+++
@@ -1,7 +0,0 @@
-using Api.Data;
-using Database.Events;
-
-namespace Cli.Endpoints.Projects.Deployments;
-
-public sealed record DeploymentQueuedEvent(
-    Deployment Entity) : IEntityDomainEvent<Deployment>;
